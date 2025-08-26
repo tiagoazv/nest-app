09112a93badefe6b2a35ffd2a80afebf0f7fa6a8
Tiago Azevedo <tiago.azevedo@olli.vc>
2025-08-26 16:32:02 -0300

    feat: add docker and typeorm to the project

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { Type } from 'class-transformer';
+import { Coffee } from './entities/coffee.entity';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Coffee])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,18 @@
+import { Column } from "typeorm/browser/decorator/columns/Column.js";
+import { PrimaryGeneratedColumn } from "typeorm/browser/decorator/columns/PrimaryGeneratedColumn.js";
+import { Entity } from "typeorm/browser/decorator/entity/Entity.js";
+
+@Entity()
 export class Coffee {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     brand: string;
+
+    @Column('json', { nullable: true })
     flavors: string[];
 }
